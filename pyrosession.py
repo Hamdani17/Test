1450f01ad5e164ebb9882f95d9da567412ffaee9
Hamdani17 <88165773+Hamdani17@users.noreply.github.com>
2023-04-30 21:27:56 +0300

    Add files via upload

---
+++ pyrosession.py
@@ -0,0 +1,24 @@
+import asyncio
+from pyrogram import Client
+
+
+async def main():
+    print("- Jmthon Session Pyrogram -")
+    print("\n---------------------------\n")
+    api_id = "15192591"
+    api_hash = "30685a4416b114f1b91921c7b30ca74a"
+    print("\n---------------------------\n")
+    async with Client(":memory:", api_id=api_id, api_hash=api_hash) as app:
+        await app.send_message(
+            "me",
+            "**كود سيشن البايروجرام**:\n\n"
+            f"`{await app.export_session_string()}`"
+        )
+        print(
+            "تم بنجاح أستخراج كود سيشن البايروجرام"
+            "ستجد الكود في الرسائل المحفوظة في التليجرام"
+        )
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
